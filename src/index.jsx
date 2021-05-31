f10f8aaf0ab1c4e438f33ec8a776b380f364826f
TarasBalem <balemtaras@gmail.com>
2021-05-31 17:31:04 +0300

    create store courses

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
