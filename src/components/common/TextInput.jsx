a1955682f04fa5e9e7f95f6ff5bd054e5417ad50
TarasBalem <balemtaras@gmail.com>
2021-06-02 11:45:48 +0300

    ready CourseForm

---
+++ src/components/common/TextInput.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextInput = ({name, label, handleChange, value, error}) => {
+  let wrapperClass = "mb-3";
+
+  if (error & (error.length > 0)) {
+    wrapperClass += " has-error";
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name} className="form-label">
+        {label}
+      </label>
+      <input
+        value={value}
+        onChange={handleChange}
+        id={name}
+        name={name}
+        type="text"
+        className="form-control"
+        placeholder={label}
+      />
+      {error && <div className="error text-danger">{error}</div>}
+    </div>
+  );
+};
+
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string.isRequired,
+};
+
+TextInput.defaultProps = {
+  value: "",
+  error: "",
+};
+
+export default TextInput;
--- src/components/courses/ManageCoursesPage.jsx
+++ src/components/courses/ManageCoursesPage.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
+import {Redirect, useParams} from "@reach/router";
 import {courseAdded} from "../../store/courses";
 import useCourses from "../../hooks/useCourses";
+import CourseForm from "./CourseForm";
 
 const newCourse = {id: null, title: "", authorId: "", category: ""};
 
@@ -10,19 +12,32 @@ const ManageCoursesPage = () => {
   const {dispatch, courses, authors} = useCourses();
 
   const handleChange = (e) => {
-    // const {name, value} = e.target;
-    // setCourse((v) => ({...v, [name]: value}));
+    const {name, value} = e.target;
+    setCourse((prev) => ({
+      ...prev,
+      [name]: name === "authorId" ? parseInt(value) : value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(course);
     // dispatch(courseAdded(course));
     // setCourse(initialCourse);
   };
 
   return (
     <div className="container mt-5">
-      <h1>Manage Course</h1>
+      <div className="row">
+        <CourseForm
+          course={course}
+          authors={authors}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          errors={errors}
+          saving={false}
+        />
+      </div>
     </div>
   );
 };
